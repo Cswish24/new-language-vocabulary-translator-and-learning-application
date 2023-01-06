fc8233fe8adc3e3027cf05290b7aaf4dd499776b
Cameron <85236636+Cswish24@users.noreply.github.com>
2023-01-06 16:38:10 -0600

    switched translate function api from ibm watson to aws translate. aws translate has much better performance and can handle phrases

--- application/translator_api.py
+++ application/translator_api.py
@@ -1,42 +1,29 @@
 from os import getenv, environ
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
+from boto3 import client
 
 
 def translate_en(word):
-    APIKEY = getenv('APIKEY')
-    URL = getenv("URL")
-    authenticator = IAMAuthenticator(APIKEY)
-    language_translator = LanguageTranslatorV3(
-        version='2018-05-01',
-        authenticator=authenticator
-    )
-
-    language_translator.set_service_url(URL)
-
-    translation = language_translator.translate(
-        text=word,
-        model_id='en-es').get_result()
-    translation = translation["translations"][0]["translation"]
+    translate = client(service_name='translate',
+                        region_name='us-west-2',
+                        use_ssl=True)
+    result = translate.translate_text(Text=word,
+                                    SourceLanguageCode="en",
+                                    TargetLanguageCode="es")
+    translation = result.get("TranslatedText")
     print(translation)
     return translation
 
 def translate_es(word):
-    APIKEY = getenv("APIKEY")
-    URL = getenv("URL")
-
-    authenticator = IAMAuthenticator(APIKEY)
-    language_translator = LanguageTranslatorV3(
-        version='2018-05-01',
-        authenticator=authenticator
-    )
-
-    language_translator.set_service_url(URL)
-
-    translation = language_translator.translate(
-        text=word,
-        model_id='es-en').get_result()
-    translation = translation["translations"][0]["translation"]
-
+    translate = client(service_name='translate',
+                        region_name='us-west-2',
+                        use_ssl=True)
+    result = translate.translate_text(Text=word,
+                                    SourceLanguageCode="es",
+                                    TargetLanguageCode="en")
+    translation = result.get("TranslatedText")
+    print(translation)
     return translation
+
+
